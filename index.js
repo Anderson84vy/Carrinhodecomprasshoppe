a7755c304089c48dce6d52b4a71d8a6ac104fae5
Anderson84vy <167568353+Anderson84vy@users.noreply.github.com>
2026-04-27 11:35:33 -0300

    Refactor index.js to initialize cart and items

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const 
+import createItem from "./services/item";
 
+const cart = []; 
 
+console.log("Welcome to the your Shoppe cart!");
 
-console.log("Fala shoppe");
+const item1 = createItem("hotwheels Omega95", 28,99, 1);
+const item2 = createItem("hotwheels Gol92", 28,99, 3);
+
+console.log(item2.subtotal());
