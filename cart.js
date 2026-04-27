5ecdbd8af342a4ee434fab26d5c0717228b003e8
Anderson84vy <167568353+Anderson84vy@users.noreply.github.com>
2026-04-27 11:00:35 -0300

    Add files via upload

---
+++ cart.js
@@ -0,0 +1,21 @@
+// Quais ações meu carrinho da shoppe pode fazer
+
+//Casos de uso
+->adicionar item no carrinho
+async function addItem(userCart){
+
+}
+    
+
+
+->deletar item do carrinho 
+async function deleteItens(userCart, name){
+
+}
+
+->remover um item
+async function removeItem(userCart, index){
+    
+}
+
+->calcular o total 
---
+++ readme.md
@@ -0,0 +1,11 @@
+## objetivo
+
+Criar um carrinho de compras basedado no carrinho de
+compras da shoppe, aonde o carrinho armazene itens e faça
+o cálculo de sub=itens automaticamente
+
+// domínio da aplicação: carrinho de compras 
+
+//as entidades representadas
+// -> carrinho = cart.js
+// -> itens = item.js
