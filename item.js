8c981c1200d08277d4b510f326e9ff0fc8c54d23
Anderson84vy <167568353+Anderson84vy@users.noreply.github.com>
2026-04-27 11:36:19 -0300

    Update item.js

--- item.js
+++ item.js
@@ -1,1 +1,13 @@
-const 
+// CASOS DE USO DE ITENS
+
+ criar itens com subtotal createContext
+ async function createItem(name, price, quantity) {
+    return {
+        name,
+        price,
+        quantity,
+        subtotal: () => price * quantity,
+    };
+ }
+
+ export default createItem; 
