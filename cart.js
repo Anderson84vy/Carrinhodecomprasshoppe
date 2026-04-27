44b557050c053f40719ba1ee7fd35e288161211e
Anderson84vy <167568353+Anderson84vy@users.noreply.github.com>
2026-04-27 11:34:35 -0300

    Add calculateTotal function and update comments

--- cart.js
+++ cart.js
@@ -1,21 +1,24 @@
 // Quais ações meu carrinho da shoppe pode fazer
 
 //Casos de uso
-->adicionar item no carrinho
+// -> adicionar item no carrinho
 async function addItem(userCart){
 
 }
     
-
-
-->deletar item do carrinho 
+// ->deletar item do carrinho 
 async function deleteItens(userCart, name){
 
 }
 
-->remover um item
+// ->remover um item - diminui um item
 async function removeItem(userCart, index){
     
 }
 
-->calcular o total 
+// -> calcular o total do carrinho 
+
+async function calculateTotal(userCart) {
+    
+}
+
