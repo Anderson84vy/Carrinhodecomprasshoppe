c64c22896f74efffb4f20a79810d40b1888ad456
Anderson84vy <167568353+Anderson84vy@users.noreply.github.com>
2026-04-28 12:20:49 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-import createItem from "./services/item";
+import * as cartSrevice from "./services/cart.js";
+import createItem from "./services/item.js";
 
-const cart = []; 
+const mycart = []; 
+const mywhishList = [];
 
 console.log("Welcome to the your Shoppe cart!");
 
 const item1 = createItem("hotwheels Omega95", 28,99, 1);
 const item2 = createItem("hotwheels Gol92", 28,99, 3);
+
+await cartService.addItem(mycart, item1);
+await cartService.addItem(mywhishList, item2);
+
+console.log("Shoppe cart TOTAL IS:");
+await cartService.calculateTotal(mycart);
+
+cartSrevice.addItem();
 
 console.log(item2.subtotal());
