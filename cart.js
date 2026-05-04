f09365dda78652d48796ddc91c15138d61fc7adb
Anderson84vy <167568353+Anderson84vy@users.noreply.github.com>
2026-05-04 08:29:03 -0300

    Refactor cart.js with improved functions and logging

--- cart.js
+++ cart.js
@@ -7,18 +7,29 @@ userCart.push(item);
 }
 
 // -> calcular o total do carrinho 
-
 async function calculateTotal(userCart) {
-    return userCart.reduce((total, item) => totaal + item.subtotal(), 0);
+
+console.log("/nShoppe Cart TOTAL IS:");
+
+const result = userCart.reduce((total, item) => total = item.subtotal(), 0);
+console.log(`🎁Total: ${result}`);
 }
+
 // ->deletar item do carrinho 
 async function deleteItens(userCart, name){
+const index = userCart.findIndex((item) => item.name === name);
 
+if (index !== -1) {
+    userCart.splice(index, 1);
+}
 }
 
 // ->remover um item - diminui um item
 async function removeItem(userCart, index){
-    
+    //1. encontrar o indice o item
+    const indexFound = userCart.findIndex((p) => p.name === item.name);
+
+    //2. Caso não encontre o item
 }
 
 export{
