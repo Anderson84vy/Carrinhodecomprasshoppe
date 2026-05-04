8b590bf73f713782462482431e516b1eabf29ece
Anderson84vy <167568353+Anderson84vy@users.noreply.github.com>
2026-05-04 08:29:33 -0300

    Implement item removal and cart display features
    Added functionality to remove items from the wishlist and display the cart contents.

--- index.js
+++ index.js
@@ -6,15 +6,19 @@ const mywhishList = [];
 
 console.log("Welcome to the your Shoppe cart!");
 
+// Criando dois itens
 const item1 = createItem("hotwheels Omega95", 28,99, 1);
 const item2 = createItem("hotwheels Gol92", 28,99, 3);
-
+// Adicione dois itens ao carrinho
 await cartService.addItem(mycart, item1);
 await cartService.addItem(mywhishList, item2);
 
-console.log("Shoppe cart TOTAL IS:");
+await cartService.removeItem(mywhishList, item2);
+await cartService.removeItem(mywhishList, item2);
+await cartService.removeItem(mywhishList, item2);
+
+await cartService.displaycart(mycart);
+// Deletei dois itens do carrinho
+// await cartService.deleteItem(myCart, item2.name);
+// await cartService.deleteItem(myCart, item1.name);
 await cartService.calculateTotal(mycart);
-
-cartSrevice.addItem();
-
-console.log(item2.subtotal());
