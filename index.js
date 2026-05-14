984a4128c2f788145a2c79bbdcafccd58f53cd8a
Anderson Gabriel <167568353+Anderson84vy@users.noreply.github.com>
2026-05-14 08:28:31 -0300

    Refactor cart and wishlist handling in index.js

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-import * as cartSrevice from "./services/cart.js";
+import * as cartService from "./services/cart.js";
 import createItem from "./services/item.js";
 
-const mycart = []; 
-const mywhishList = [];
+const myCart = [];
+const myWishList = [];
 
-console.log("Welcome to the your Shoppe cart!");
+console.log("Welcome to your Shoppe cart!");
 
 // Criando dois itens
-const item1 = createItem("hotwheels Omega95", 28,99, 1);
-const item2 = createItem("hotwheels Gol92", 28,99, 3);
-// Adicione dois itens ao carrinho
-await cartService.addItem(mycart, item1);
-await cartService.addItem(mywhishList, item2);
+const item1 = createItem("Hotwheels Omega95", 28.99, 1);
+const item2 = createItem("Hotwheels Gol92", 28.99, 3);
 
-await cartService.removeItem(mywhishList, item2);
-await cartService.removeItem(mywhishList, item2);
-await cartService.removeItem(mywhishList, item2);
+// Adicionando itens
+await cartService.addItem(myCart, item1);
+await cartService.addItem(myWishList, item2);
 
-await cartService.displaycart(mycart);
-// Deletei dois itens do carrinho
-// await cartService.deleteItem(myCart, item2.name);
-// await cartService.deleteItem(myCart, item1.name);
-await cartService.calculateTotal(mycart);
+// Removendo itens da lista de desejos
+await cartService.removeItem(myWishList, item2);
+await cartService.removeItem(myWishList, item2);
+await cartService.removeItem(myWishList, item2);
+
+// Mostrando carrinho
+await cartService.displayCart(myCart);
+
+// Calculando total
+await cartService.calculateTotal(myCart);
