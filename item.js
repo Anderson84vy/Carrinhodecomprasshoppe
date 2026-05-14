ec5c72804eb9ce4625792ef77a5b67a266d543fc
Anderson Gabriel <167568353+Anderson84vy@users.noreply.github.com>
2026-05-14 08:31:34 -0300

    Refactor createItem function subtotal calculation
    Refactor subtotal method to use 'this' context.

--- item.js
+++ item.js
@@ -1,13 +1,15 @@
 // CASOS DE USO DE ITENS
 
- // -> criar itens com subtotal createContext
- async function createItem(name, price, quantity) {
-    return {
-        name,
-        price,
-        quantity,
-        subtotal: () => price * quantity,
-    };
- }
+// -> criar itens com subtotal
+async function createItem(name, price, quantity) {
+  return {
+    name,
+    price,
+    quantity,
+    subtotal: function () {
+      return this.price * this.quantity;
+    },
+  };
+}
 
- export default createItem; 
+export default createItem;
